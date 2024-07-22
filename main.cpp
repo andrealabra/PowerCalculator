5e80ab4e2047e1d98515753562e68407f137d347
Andrea Labra Orozco <65309888+andrealabra@users.noreply.github.com>
2024-07-22 14:30:53 -0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std; 
+
+int main() {
+    double result = pow(9, 2);
+
+    // Output the result
+    cout << "9^9 = " << result << endl;
+
+    return 0;
+}
